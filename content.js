77a473c90fc8e0e728d0dd64f12d339eb68498bb
David Barragán Merino <david.barragan@kaleidos.net>
2016-05-28 20:16:49 +0200

    Initial version :baby:

---
+++ content.js
@@ -0,0 +1,4 @@
+
+findAndReplaceDOMText(document.body, { find: "Venezuela",    replace: 'Chiquitistán'});
+findAndReplaceDOMText(document.body, { find: "venezolano",    replace: 'chiquitistaní'});
+findAndReplaceDOMText(document.body, { find: "venezolana",    replace: 'chiquitistanita'});
