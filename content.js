c42197d111ff4271a4c7e325c692067a5a622586
David Barragán Merino <david.barragan@kaleidos.net>
2016-05-29 02:24:23 +0200

    Add more replacements

--- content.js
+++ content.js
@@ -1,4 +1,22 @@
 
-findAndReplaceDOMText(document.body, { find: "Venezuela",    replace: 'Chiquitistán'});
-findAndReplaceDOMText(document.body, { find: "venezolano",    replace: 'chiquitistaní'});
-findAndReplaceDOMText(document.body, { find: "venezolana",    replace: 'chiquitistanita'});
+replacements = [
+    {'find': 'Venezuela',        'replace': 'Chiquitistán'},
+    {'find': 'VENEZUELA',        'replace': 'CHIQUITISTÁN'},
+    {'find': 'venezuela',        'replace': 'chiquitistán'},
+
+    {'find': 'venezolanos',      'replace': 'chiquitistanies'},
+    {'find': 'VENEZOLANOS',      'replace': 'CHIQUITISTANIES'},
+
+    {'find': 'venezolano',       'replace': 'chiquitistaní'},
+    {'find': 'VENEZOLANO',       'replace': 'CHIQUITISTANÍ'},
+
+    {'find': 'venezolanas',      'replace': 'chiquitistanitas'},
+    {'find': 'VENEZOLANAS',      'replace': 'CHIQUITISTANITAS'},
+
+    {'find': 'venezolana',       'replace': 'chiquitistanita'},
+    {'find': 'VENEZOLANA',       'replace': 'CHIQUITISTANITA'},
+];
+
+replacements.forEach(function(elem, idx) {
+    findAndReplaceDOMText(document.body, {'find': elem.find, 'replace': elem.replace});
+});
