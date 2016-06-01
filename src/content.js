b7df8089f3c2ec9ffbe44486f3f90cf23b3a040e
David <davitomhq@gmail.com>
2016-06-01 15:28:36 +0200

    Add more replacements Maduro/Chiquito, chavismo/chiquitismo

--- src/content.js
+++ src/content.js
@@ -15,6 +15,22 @@ replacements = [
 
     {'find': 'venezolana',       'replace': 'chiquitistanita'},
     {'find': 'VENEZOLANA',       'replace': 'CHIQUITISTANITA'},
+
+	{'find': 'Nicolas Maduro',   'replace': 'Chiquito de la Calzada'},
+	{'find': 'nicolas naduro',   'replace': 'chiquito de la calzada'},
+	{'find': 'NICOLAS MADURO',   'replace': 'CHIQUITO DE LA CALZADA'},
+
+	{'find': 'N.MADURO',         'replace': 'C. DE LA CALZADA'},
+	{'find': 'n.maduro',         'replace': 'c. de la calzada'},
+	{'find': 'N.Maduro',         'replace': 'C. de La Calzada'},
+
+	{'find': 'MADURO',           'replace': 'CHIQUITO'},
+	{'find': 'maduro',           'replace': 'chiquito'},
+	{'find': 'Maduro',           'replace': 'Chiquito'},
+
+	{'find': 'chavismo',         'replace': 'chiquitismo'},
+	{'find': 'CHAVISMO',         'replace': 'chiquitismo'},
+	{'find': 'Chavismo',         'replace': 'Chiquitismo'}
 ];
 
 replacements.forEach(function(elem, idx) {
