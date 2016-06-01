759081007420db37ddb6e32adb751f4e36bca974
David <davitomhq@gmail.com>
2016-06-01 15:28:36 +0200

    add more replacements Maduro/Chiquito

--- src/content.js
+++ src/content.js
@@ -15,6 +15,18 @@ replacements = [
 
     {'find': 'venezolana',       'replace': 'chiquitistanita'},
     {'find': 'VENEZOLANA',       'replace': 'CHIQUITISTANITA'},
+	
+	{'find': 'Nicolas Maduro',       'replace': 'Chiquito de la Calzada'},
+	{'find': 'nicolas naduro',       'replace': 'chiquito de la calzada'},
+	{'find': 'NICOLAS MADURO',       'replace': 'CHIQUITO DE LA CALZADA'},
+	
+	{'find': 'N.MADURO',       'replace': 'C. de la CALZADA'},
+	{'find': 'n.maduro',       'replace': 'c. de la calzada'},
+	{'find': 'N.Maduro',       'replace': 'C. de la Calzada'},
+	
+	{'find': 'MADURO',       'replace': 'CHIQUITO'},
+	{'find': 'maduro',       'replace': 'chiquito'},
+	{'find': 'Maduro',       'replace': 'Chiquito'}
 ];
 
 replacements.forEach(function(elem, idx) {
