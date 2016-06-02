ad69054b004a21a5b88144f13d49cc7f79e19d8b
David Barragán Merino <david.barragan@kaleidos.net>
2016-06-02 19:03:02 +0200

    Change more tabs by 4 spaces

--- src/content.js
+++ src/content.js
@@ -1,4 +1,3 @@
-
 replacements = [
     {'find': 'Venezuela',        'replace': 'Chiquitistán'},
     {'find': 'VENEZUELA',        'replace': 'CHIQUITISTÁN'},
@@ -16,21 +15,21 @@ replacements = [
     {'find': 'venezolana',       'replace': 'chiquitistanita'},
     {'find': 'VENEZOLANA',       'replace': 'CHIQUITISTANITA'},
 
-	{'find': 'Nicolas Maduro',   'replace': 'Chiquito de la Calzada'},
-	{'find': 'nicolas naduro',   'replace': 'chiquito de la calzada'},
-	{'find': 'NICOLAS MADURO',   'replace': 'CHIQUITO DE LA CALZADA'},
+    {'find': 'Nicolas Maduro',   'replace': 'Chiquito de la Calzada'},
+    {'find': 'nicolas naduro',   'replace': 'chiquito de la calzada'},
+    {'find': 'NICOLAS MADURO',   'replace': 'CHIQUITO DE LA CALZADA'},
 
-	{'find': 'N.MADURO',         'replace': 'C. DE LA CALZADA'},
-	{'find': 'n.maduro',         'replace': 'c. de la calzada'},
-	{'find': 'N.Maduro',         'replace': 'C. de La Calzada'},
+    {'find': 'N.MADURO',         'replace': 'C. DE LA CALZADA'},
+    {'find': 'n.maduro',         'replace': 'c. de la calzada'},
+    {'find': 'N.Maduro',         'replace': 'C. de La Calzada'},
 
-	{'find': 'MADURO',           'replace': 'CHIQUITO'},
-	{'find': 'maduro',           'replace': 'chiquito'},
-	{'find': 'Maduro',           'replace': 'Chiquito'},
+    {'find': 'MADURO',           'replace': 'CHIQUITO'},
+    {'find': 'maduro',           'replace': 'chiquito'},
+    {'find': 'Maduro',           'replace': 'Chiquito'},
 
-	{'find': 'chavismo',         'replace': 'chiquitismo'},
-	{'find': 'CHAVISMO',         'replace': 'chiquitismo'},
-	{'find': 'Chavismo',         'replace': 'Chiquitismo'}
+    {'find': 'chavismo',         'replace': 'chiquitismo'},
+    {'find': 'CHAVISMO',         'replace': 'chiquitismo'},
+    {'find': 'Chavismo',         'replace': 'Chiquitismo'}
 ];
 
 replacements.forEach(function(elem, idx) {
